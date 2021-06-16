3f7e1f5e0918e0d0ab1070cae9e12913da64412e
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-16 15:03:13 +0100

    Add each new router to the index of routes folder and export

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 const seasonRouter = require('./season');
+const playerRouter = require('./player');
+const teamRouter = require('./team');
+const franchiseListRouter = require('./franchiseList');
+const franchiseIndexRouter = require('./franchiseIndex');
+const seasonIndexRouter = require('./seasonIndex');
 
 module.exports = async (app) => {
   seasonRouter(app);
+  playerRouter(app);
+  teamRouter(app);
+  franchiseListRouter(app);
+  franchiseIndexRouter(app);
+  seasonIndexRouter(app);
 };
