5e76cbde6edb58d21117c4e3dd6fdf782c7f1418
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-18 17:23:08 +0100

    Add endpoints that return full season data (overall, rs and po) for a single year and league

--- routes/season.js
+++ routes/season.js
@@ -37,6 +37,60 @@ module.exports = (app) => {
         res.status(500).send('Error: ' + e);
       });
   });
+
+  // Get all player records for single season by year and league and order by season value
+  router.get('/overall/:league/:year', (req, res) => {
+    Seasons.find({
+      year: req.params.year,
+      league: req.params.league
+    })
+      .sort({ season_value: -1 })
+      .then((season) => {
+        res.status(200).json(season);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500).send('Error: ' + e);
+      });
+  });
+
+  // Get all player records for single season by year and league and order by rs score
+  router.get('/regularseason/:league/:year', (req, res) => {
+    Seasons.find({
+      year: req.params.year,
+      league: req.params.league,
+      // Make sure that regular season minutes are greater than 0
+      rs_mp: { $ne: 0 }
+    })
+      .sort({ rs_score: -1 })
+      .then((season) => {
+        res.status(200).json(season);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500).send('Error: ' + e);
+      });
+  });
+
+  // Get all player records for single season by year and league and order by rs score
+  router.get('/playoffs/:league/:year', (req, res) => {
+    Seasons.find({
+      year: req.params.year,
+      league: req.params.league,
+      // Make sure that po team exists (string length is >= 1)
+      po_tm: { $exists: true },
+      $expr: { $gte: [{ $strLenCP: '$po_tm' }, 1] }
+    })
+      .sort({ po_score: -1 })
+      .then((season) => {
+        res.status(200).json(season);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500).send('Error: ' + e);
+      });
+  });
+
   // Get all season records and order to return top 100 full season scores
   router.get('/rankings/overall', (req, res) => {
     Seasons.find()
