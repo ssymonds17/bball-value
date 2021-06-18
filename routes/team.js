640559c3581fd2c039f4eb69d4ab7b327c583666
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-06-18 14:45:55 +0100

    Add new route to return single team record based on ID and year

--- routes/team.js
+++ routes/team.js
@@ -6,6 +6,21 @@ const Teams = Models.Team;
 module.exports = (app) => {
   app.use('/api/v1/teams', router);
 
+  // Get a single team record using franchise code and year
+  router.get('/:franchiseCode/:year', (req, res) => {
+    Teams.find({
+      franchise_code: req.params.franchiseCode,
+      year: req.params.year
+    })
+      .then((team) => {
+        res.status(200).json(team);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500).send('Error: ' + e);
+      });
+  });
+
   // Get all time teams list and limit to 100
   router.get('/greatest', (req, res) => {
     Teams.find()
